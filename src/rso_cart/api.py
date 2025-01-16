c5b18c62bfb4e62bfb1621e8d9be1d947cbad92c
Iztok Bajcar <iztok.bajcar@gmail.com>
2025-01-16 23:46:09 +0100

    Updated endpoints

--- src/rso_cart/api.py
+++ src/rso_cart/api.py
@@ -65,13 +65,47 @@ async def add_to_cart(tenant, uid, pid):
     return new_cart_info.to_dict()
 
 
-@app.delete("/{tenant}/cart/{uid}/{pid}")
+@app.put("/{tenant}/cart/{uid}/{pid}")
 async def delete_from_cart(tenant, uid, pid):
     logger.info(f"DELETE /{tenant}/cart/{uid}/{pid}")
     db_name = f"rso_shop_{tenant}"
     logger.info(f"Using database: {db_name}")
     db_conn = connect_to_database("mongo", db_name)
     cart_info = get_cart_info(db_conn, uid)
+    new_cart_info = decrease_quantity_of_product_in_cart(cart_info, pid)
+    new_contents = new_cart_info.get_contents()
+
+    cart = db_conn["cart"].find_one({"user_id": uid})
+    if cart is None:
+        db_conn["cart"].insert_one({"user_id": uid, "contents": new_contents})
+    else:
+        query = {"user_id": uid}
+        new_value = {"$set": {"user_id": uid, "contents": new_contents}}
+
+        db_conn["cart"].update_one(query, new_value)
+
+    return new_cart_info.to_dict()
+
+
+@app.delete("/{tenant}/cart/{uid}")
+async def delete_cart(tenant, uid):
+    logger.info(f"DELETE /{tenant}/cart/{uid}")
+    db_name = f"rso_shop_{tenant}"
+    logger.info(f"Using database: {db_name}")
+    db_conn = connect_to_database("mongo", db_name)
+    db_conn["cart"].delete_one({"user_id": uid})
+    return {"status": "Cart deleted"}
+
+
+@app.delete("/{tenant}/cart/{uid}/{pid}")
+async def delete_product_from_cart(tenant, uid, pid):
+    logger.info(f"DELETE /{tenant}/cart/{uid}/{pid}")
+    db_name = f"rso_shop_{tenant}"
+    logger.info(f"Using database: {db_name}")
+    db_conn = connect_to_database("mongo", db_name)
+    cart_info = get_cart_info(db_conn, uid)
+
+    # remove the product altogether from the dictionary
     new_cart_info = decrease_quantity_of_product_in_cart(cart_info, pid)
     new_contents = new_cart_info.get_contents()
 
